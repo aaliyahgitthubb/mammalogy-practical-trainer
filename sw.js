bf7433f9c84d678467027c6b50f2fd308c78766a
aaliyahgitthubb <aaliyahnews07@gmail.com>
2026-09-21 22:04:16 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='mammal-vault-2026.09.21.2';
-const CORE=['./','./index.html','./styles.css','./app.js?v=1.7.1','./data.js','./embedded_images.js','./manifest.webmanifest','./version.json'];
+const CACHE='mammal-vault-2026.09.22.1';
+const CORE=['./','./index.html','./styles.css','./app.js?v=1.8.0','./data.js','./embedded_images.js','./manifest.webmanifest','./version.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
